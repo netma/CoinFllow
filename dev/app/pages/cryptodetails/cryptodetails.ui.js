d6bdfaaf2adbfb1df00e9176b0254ed2acf54c85
Ateliers Nomades <info@iMac-de-Ateliers.local>
2017-12-21 19:48:55 +0100

    Update header and style

--- dev/app/pages/cryptodetails/cryptodetails.ui.js
+++ dev/app/pages/cryptodetails/cryptodetails.ui.js
@@ -1,31 +1,25 @@
 export function cryptodetailsSkeleton(data) {
   return `
     <section class="cryptodetails">
-      <header>
-        <ul id="dropdown1" class="dropdown-content">
-          <li>Connected as ${data.userEmail}</li>
-          <li><a href="" class="logout">Logout</a></li>
-        </ul>
-        <nav class="light-blue lighten-3">
-          <div class="nav-wrapper">
-            <a href="" class="brand-logo left" alt=""><img src="../src/img/CoinFllow.svg">CoinFllow</a>
-            <a href="" data-activates="mobile-demo" class="button-collapse right"><i class="material-icons">menu</i></a>
-            <ul class="right hide-on-med-and-down">
-              <li><a href="" class="watchlist">Watch List</a></li>
-              <li><a href="" class="portfolio">Portfolio</a></li>
-              <li><a class="dropdown-button right" href="" data-activates="dropdown1"><i class="material-icons medium right">person</i></a></li>
-            </ul>
-            <ul class="side-nav" id="mobile-demo">
-              <li><span><i class="material-icons small">person</i><span></li>
-              <li><span>${data.userEmail}<span></li>
-              <li><a class="logout" href="">Logout</a></li>
-              <li class="divider"></li>
-              <li><a href="" class="watchlist">Watch List</a></li>
-              <li><a href="" class="portfolio">Portfolio</a></li>
-            </ul>
-          </div>
-        </nav>
-      </header>
+    <header>
+      <ul id="dropdown1" class="dropdown-content">
+        <li><a href="" class="logout">Logout</a></li>
+      </ul>
+      <nav class="light-blue lighten-3">
+        <div class="nav-wrapper">
+          <a class="brand-logo left" alt=""><img src="../src/img/CoinFllow.svg">CoinFllow</a>
+          <a href="" data-activates="mobile-demo" class="button-collapse right"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a class="dropdown-button right" href="" data-activates="dropdown1"><i class="material-icons medium left">person</i>${data.userEmail}</a></li>
+          </ul>
+          <ul class="side-nav" id="mobile-demo">
+            <li><span><i class="material-icons small">person</i><span></li>
+            <li><span>${data.userEmail}<span></li>
+            <li><a class="logout" href="">Logout</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
       <div id="content">
         <a href="" id="cryptodetails-close" class="right"><i class="material-icons">clear</i><a>
         <ul id="cryptodetails" class="collection with-header"></ul>
--- dev/app/pages/watchlist/watchlist.ui.js
+++ dev/app/pages/watchlist/watchlist.ui.js
@@ -3,25 +3,19 @@ export function watchlistSkeleton(data) {
     <section class="watchlist">
       <header>
         <ul id="dropdown1" class="dropdown-content">
-          <li>Connected as ${data.userEmail}</li>
           <li><a href="" class="logout">Logout</a></li>
         </ul>
         <nav class="light-blue lighten-3">
           <div class="nav-wrapper">
-            <a href="" class="brand-logo left" alt=""><img src="../src/img/CoinFllow.svg">CoinFllow</a>
+            <a class="brand-logo left" alt=""><img src="../src/img/CoinFllow.svg">CoinFllow</a>
             <a href="" data-activates="mobile-demo" class="button-collapse right"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
-              <li><a href="" class="watchlist">Watch List</a></li>
-              <li><a href="" class="portfolio">Portfolio</a></li>
-              <li><a class="dropdown-button right" href="" data-activates="dropdown1"><i class="material-icons medium right">person</i></a></li>
+              <li><a class="dropdown-button right" href="" data-activates="dropdown1"><i class="material-icons medium left">person</i>${data.userEmail}</a></li>
             </ul>
             <ul class="side-nav" id="mobile-demo">
               <li><span><i class="material-icons small">person</i><span></li>
               <li><span>${data.userEmail}<span></li>
               <li><a class="logout" href="">Logout</a></li>
-              <li class="divider"></li>
-              <li><a href="" class="watchlist">Watch List</a></li>
-              <li><a href="" class="portfolio">Portfolio</a></li>
             </ul>
           </div>
         </nav>
